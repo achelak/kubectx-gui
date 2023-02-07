be8b025dca5f8dbcb3eab196c5b4dc3263d80e4e
a.chelak <a.chelak@autodc.eu>
2023-02-08 01:34:22 +0200

    Added the exit button.

--- kubectx-gui.py
+++ kubectx-gui.py
@@ -7,10 +7,13 @@ icon = Image.open("/opt/kubectx-gui/kubectx-gui.png")
 
 
 def switch_context(icon, item):
-    subprocess.run(["kubectx", str(item)], stderr=subprocess.DEVNULL)
-    current_context = subprocess.run(["kubectx", "-c"], stdout=subprocess.PIPE, text=True)
-    message = str("The k8s context has been switched to:\n") + str(current_context.stdout)
-    os.system('notify-send "Kubectx!" "'+message+'"')
+    if str(item) == "Exit":
+        icon.stop()
+    else:
+        subprocess.run(["kubectx", str(item)], stderr=subprocess.DEVNULL)
+        current_context = subprocess.run(["kubectx", "-c"], stdout=subprocess.PIPE, text=True)
+        message = str("The k8s context has been switched to:\n") + str(current_context.stdout)
+        os.system('notify-send "Kubectx!" "'+message+'"')
 
 
 contexts = subprocess.run(["kubectx"], stdout=subprocess.PIPE, text=True)
@@ -19,6 +22,8 @@ menu_items = []
 
 for context in contexts.stdout.split("\n")[:-1]:
     menu_items.append(pystray.MenuItem(context, switch_context))
+
+menu_items.append(pystray.MenuItem("Exit", switch_context))
 
 menu = pystray.Menu(*menu_items)
 
